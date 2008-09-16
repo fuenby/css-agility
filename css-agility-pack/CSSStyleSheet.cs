552cd7709019d247fb9526110b3ac29e0f7c371f
Mihai Ciumeica <cmihai@gmail.com>
2008-09-16 18:06:31 +0300

    Initial commit

---
+++ css-agility-pack/CSSStyleSheet.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace CSSAgilityPack
+{
+    public class CSSStyleSheet
+    {
+        void Load(string fileName)
+        {
+            Load(new StreamReader(fileName));
+        }
+
+        void Load(StreamReader r)
+        {
+            text_ = r.ReadToEnd();
+
+            Parse();
+        }
+
+        public string text_;
+        public int pos_;
+
+        public List<CSSRule> Parse(string what)
+        {
+            text_ = what; pos_ = 0;
+            return Parse();
+        }
+
+        public List<CSSRule> Parse()
+        {
+            var result = new List<CSSRule>();
+
+            pos_ = 0;
+            while (pos_ < text_.Length)
+            {
+                CSSRule r = ParseRule();
+                if (r != null)
+                {
+                    result.Add(r);
+                    continue;
+                }
+                
+                if (AcceptCDO())
+                    continue;
+
+                if (AcceptCDC())
+                    continue;
+
+                AcceptWhitespace();
+            }
+
+            return result;
+        }
+
+        public CSSRule ParseRule()
+        {
+            int mark = pos_;
+
+            List<string> selectors = new List<string>();
+
+            string s = ParseSelector();
+            while (s != null)
+            {
+                selectors.Add(s);
+                s = ParseSelector();
+            }
+            if (selectors.Count == 0)
+                return null;
+
+            AcceptWhitespace();
+
+            if (!AcceptChar('{'))
+            {
+                pos_ = mark;
+                return null;
+            }
+
+            CSSBlock b = ParseBlock();
+            if (b == null)
+            {
+                pos_ = mark;
+                return null;
+            }
+
+            AcceptWhitespace();
+            if (!AcceptChar('}'))
+            {
+                pos_ = mark;
+                return null;
+            }
+            
+            return new CSSRule(selectors, b);
+        }
+
+        public string ParseSelector()
+        {
+            return ParseWord();
+        }
+
+        public string ParseWord()
+        {
+            int mark = pos_;
+            StringBuilder result = new StringBuilder();
+
+            AcceptWhitespace();
+            while (pos_ < text_.Length)
+            {
+                if (!char.IsLetterOrDigit(text_[pos_]))
+                    break;
+
+                result.Append(text_[pos_++]);
+            }
+            AcceptWhitespace();
+
+            if (result.Length == 0)
+            {
+                pos_ = mark;
+                return null;
+            }
+
+            return result.ToString();
+        }
+
+        public CSSBlock ParseBlock()
+        {
+            int mark = pos_;
+
+            var decls = new List<CSSDeclaration>();
+
+            while (true)
+            {
+                var decl = ParseDeclaration();
+                if (decl != null)
+                    decls.Add(decl);
+
+                if (!AcceptChar(';'))
+                    break;
+            }
+
+            return new CSSBlock(decls);
+        }
+
+        CSSDeclaration ParseDeclaration()
+        {
+            int mark = pos_;
+
+            string attr = ParseAttribute();
+            if (attr == null)
+            {
+                pos_ = mark;
+                return null;
+            }
+
+            if (!AcceptChar(':'))
+            {
+                pos_ = mark;
+                return null;
+            }
+
+            string value = ParseValue();
+            if (value == null)
+            {
+                pos_ = mark;
+                return null;
+            }
+
+            return new CSSDeclaration(attr, value);
+        }
+
+        string ParseAttribute()
+        {
+            return ParseWord();
+        }
+
+        string ParseValue()
+        {
+            return ParseWord();
+        }
+
+        public bool AcceptCDO()
+        {
+            return AcceptString("<!--");
+        }
+
+        public bool AcceptCDC()
+        {
+            return AcceptString("-->");
+        }
+
+        public bool AcceptString(string what)
+        {
+            int mark = pos_;
+
+            int i = 0;
+            while (pos_ < text_.Length && i < what.Length && text_[pos_] == what[i])
+            {
+                pos_++; i++;
+            }
+
+            if (i == what.Length)
+                return true;
+
+            pos_ = mark;
+            return false;
+        }
+
+        public bool AcceptChar(char ch)
+        {
+            if (pos_ < text_.Length && text_[pos_] == ch)
+            {
+                pos_++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void AcceptWhitespace()
+        {
+            while (pos_ < text_.Length && char.IsWhiteSpace(text_[pos_]))
+            {
+               pos_++;
+            }
+        }
+    }
+
+    public class Token
+    {
+        public static Token Valid = new Token();
+    };
+
+    public class CSSBlock
+    {
+        List<CSSDeclaration> decls_;
+
+        public CSSBlock(List<CSSDeclaration> decls)
+        {
+            decls_ = decls;
+        }
+    }
+
+    public class CSSRule
+    {
+        public CSSRule(List<string> selectors, CSSBlock block)
+        {
+            selectors_ = selectors;
+            block_ = block;
+        }
+
+        List<string> selectors_;
+        CSSBlock block_;
+    }
+
+    public class CSSDeclaration
+    {
+        string attr_, value_;
+
+        public CSSDeclaration(string attr, string value)
+        {
+            attr_ = attr;
+            value_ = value;
+        }
+    }
+}
---
+++ css-agility-pack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace CSSAgilityPack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSSStyleSheet c = new CSSStyleSheet();
+
+            Debug.Assert(c.Parse("     \t\n\t   ") != null);
+            Debug.Assert(c.Parse("<!-- -->") != null);
+            Debug.Assert(c.Parse("body {color: red; }") != null);
+            Debug.Assert(c.Parse("body {;;}") != null);
+        }
+    }
+}
