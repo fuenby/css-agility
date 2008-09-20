58fe8a8798a7415122af85852c9ffcaaffb1f02d
Alex Fuenby <fuenby@gmail.com>
2008-09-20 12:33:00 +0300

    Added more value types

--- TestSuite/Program.cs
+++ TestSuite/Program.cs
@@ -23,23 +23,44 @@ namespace TestSuite
         [Test]
         public void MultipleInlineStyles()
         {
-            CSSStyleDeclaration s = CSSParser.ParseStyleDeclaration("color: red; font-family: 'Helvetica', \"Arial\", sans-serif;" + 
-            "font-style: italic;");
+            CSSStyleDeclaration s = CSSParser.ParseStyleDeclaration("color: red; font-family: 'Helvetica'; font-style: italic;");
 
             Assert.AreEqual(3, s.Count);
             Assert.AreEqual("italic", s.GetPropertyValue("font-style"));
         }
 
+        [Test]
+        public void SpaceSeparatedValues()
+        {
+            CSSStyleDeclaration s = CSSParser.ParseStyleDeclaration("border: 1px solid gold;color: blue");
+
+            Assert.AreEqual(2, s.Count);
+            Assert.AreEqual("blue", s.GetPropertyValue("color"));
+        }
+
+        [Test]
+        public void Whitespace()
+        {
+            CSSStyleDeclaration s = CSSParser.ParseStyleDeclaration(" border : 1px solid ; color : \r\tblue ");
+
+            Assert.AreEqual(2, s.Count);
+            Assert.AreEqual("blue", s.GetPropertyValue("color"));
+            Assert.AreEqual("1px solid", s.GetPropertyValue("border"));
+        }
+
+        [Test]
+        public void NonStandardUnits()
+        {
+            CSSStyleDeclaration s = CSSParser.ParseStyleDeclaration("font-size: x-large; height: 80%; line-height: 12.7em; ");
+
+            Assert.AreEqual(3, s.Count);
+        }
+
         public static void Main(string[] args)
         {
-            HtmlDocument d = new HtmlDocument();
-
-            d.Load("test.html");
-            HtmlNode p = d.GetElementbyId("test");
-
-            CSSStyleDeclaration s = p.Style;
+            CSSStyleDeclaration s = CSSParser.ParseStyleDeclaration("font-size: x-large; height: 80%; line-height: 12.7em; ");
             for (int i = 0; i < s.Count; ++i)
-                Console.WriteLine(s[i]);
+                Console.WriteLine("{0}: {1}", s[i], s.GetPropertyValue(s[i]));
 
             Console.WriteLine(s.Count);
         }
--- css-agility-pack/CSSParser.cs
+++ css-agility-pack/CSSParser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace CSSAgilityPack
 {
@@ -55,7 +56,7 @@ namespace CSSAgilityPack
 
             AcceptWhitespace(what, ref pos, ref end);
 
-            if (!AcceptChar('{', what, ref pos, ref end))
+            if (!AcceptChar('{', what, ref pos))
             {
                 pos = mark;
                 return null;
@@ -69,7 +70,7 @@ namespace CSSAgilityPack
             }
 
             AcceptWhitespace(what, ref pos, ref end);
-            if (!AcceptChar('}', what, ref pos, ref end))
+            if (!AcceptChar('}', what, ref pos))
             {
                 pos = mark;
                 return null;
@@ -102,7 +103,7 @@ namespace CSSAgilityPack
                 if (decl != null)
                     decls.Add(decl);
 
-                if (!AcceptChar(';', what, ref pos, ref end))
+                if (!AcceptChar(';', what, ref pos))
                     break;
             }
 
@@ -124,7 +125,7 @@ namespace CSSAgilityPack
 
             AcceptWhitespace(what, ref pos, ref end);
 
-            if (!AcceptChar(':', what, ref pos, ref end))
+            if (!AcceptChar(':', what, ref pos))
             {
                 pos = mark;
                 return null;
@@ -149,29 +150,191 @@ namespace CSSAgilityPack
 
         static string ParseValue(string what, ref int pos, ref int end)
         {
-            return ParseIdent(what, ref pos, ref end);
+            StringBuilder result = new StringBuilder();
+
+            while (true)
+            {
+                string t = ParseAny(what, ref pos, ref end);
+                if (t != null)
+                {
+                    if (result.Length != 0)
+                        result.Append(' ');
+                    result.Append(t);
+                }
+                else
+                    break;
+            }
+
+            return result.ToString();
+        }
+
+        static string ParseAny(string what, ref int pos, ref int end)
+        {
+            string result = ParseIdent(what, ref pos, ref end)
+                ?? ParseString(what, ref pos, ref end)
+                ?? ParsePercentage(what, ref pos, ref end)
+                ?? ParseDimension(what, ref pos)
+                ?? ParseNumber(what, ref pos);
+            if (result != null)
+                AcceptWhitespace(what, ref pos, ref end);
+
+            return result;
         }
 
         public static string ParseIdent(string what, ref int pos, ref int end)
         {
+            int ps = pos;
+            StringBuilder result = new StringBuilder();
+
+            if (AcceptChar('-', what, ref ps))
+                result.Append('-');
+
+            if (!AcceptNmstart(what, ref ps, ref end))
+                return null;
+            result.Append(what[ps - 1]);
+
+            while (AcceptNmchar(what, ref ps, ref end))
+                result.Append(what[ps - 1]);
+
+            pos = ps;
+            return result.ToString();
+        }
+
+        public static string ParseString(string what, ref int pos, ref int end)
+        {
             int mark = pos;
             StringBuilder result = new StringBuilder();
 
-            while (pos < end)
-            {
-                if (!char.IsLetterOrDigit(what[pos]) && what[pos] != '-' && what[pos] != '_')
-                    break;
+            if (pos >= end)
+                return null;
 
+            char sep = what[pos];
+            if (sep != '\'' && sep != '"')
+                return null;
+
+            AcceptChar(sep, what, ref pos);
+            
+            result.Append(sep);
+            while (pos < end && "\n\t\r".IndexOf(what[pos]) < 0 && what[pos] != sep)
+            {
                 result.Append(what[pos++]);
             }
 
-            if (result.Length == 0)
+            if (!AcceptChar(sep, what, ref pos))
             {
                 pos = mark;
                 return null;
             }
 
+            result.Append(sep);
             return result.ToString();
+        }
+
+        static string ParseDimension(string what, ref int pos)
+        {
+            int ps = pos;
+            int en = what.Length;
+            StringBuilder result = new StringBuilder();
+
+            string num = ParseNumber(what, ref ps);
+            if (num == null)
+                return null;
+            result.Append(num);
+
+            string dim = ParseIdent(what, ref ps, ref en);
+            if (dim == null)
+                return null;
+            result.Append(dim);
+
+            pos = ps;
+            return result.ToString();
+        }
+
+        static string ParsePercentage(string what, ref int pos, ref int end)
+        {
+            int ps = pos;
+            StringBuilder result = new StringBuilder();
+
+            while (ps < end && char.IsDigit(what[ps]))
+                result.Append(what[ps++]);
+
+            if (result.Length == 0)
+                return null;
+
+            if (!AcceptChar('%', what, ref ps))
+                return null;
+
+            result.Append('%');
+            pos = ps;
+            return result.ToString();
+        }
+
+        static string ParseNumber(string what, ref int pos)
+        {
+            int ps = pos;
+            StringBuilder result = new StringBuilder();
+            
+            while (AcceptEither("0-9", what, ref ps))
+                result.Append(what[ps - 1]);
+
+            if (AcceptChar('.', what, ref ps))
+            {
+                result.Append('.');
+                if (!AcceptEither("0-9", what, ref ps))
+                    return null;
+                while (AcceptEither("0-9", what, ref ps))
+                    result.Append(what[ps - 1]);
+            }
+
+            if (result.Length == 0)
+                return null;
+
+            pos = ps;
+            return result.ToString();
+        }
+
+        //static string ParseDimension(string what, ref int pos)
+        //{
+        //    int ps = pos;
+
+        //}
+
+        static bool AcceptNmstart(string what, ref int pos, ref int end)
+        {
+            return AcceptEither("_a-z", what, ref pos);
+        }
+
+        static bool AcceptNmchar(string what, ref int pos, ref int end)
+        {
+            return AcceptEither("-_a-z", what, ref pos);
+        }
+
+        static bool AcceptEither(string e, string what, ref int pos)
+        {
+            if (pos >= what.Length)
+                return false;
+
+            for (int i = 0; i < e.Length; ++i)
+            {
+                if (e[i] == '-' && i != 0)
+                {
+                    if (e[i-1] <= what[pos] && what[pos] <= e[i+1])
+                    {
+                        pos++;
+                        return true;
+                    }
+                }
+                else 
+                {
+                    if (what[pos] == e[i])
+                    {
+                        pos++;
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
 
         static public bool AcceptCDO(string what, ref int pos, ref int end)
@@ -201,9 +364,9 @@ namespace CSSAgilityPack
             return false;
         }
 
-        public static bool AcceptChar(char ch, string what, ref int pos, ref int end)
+        public static bool AcceptChar(char ch, string what, ref int pos)
         {
-            if (pos < end && what[pos] == ch)
+            if (pos < what.Length && what[pos] == ch)
             {
                 pos++;
                 return true;
